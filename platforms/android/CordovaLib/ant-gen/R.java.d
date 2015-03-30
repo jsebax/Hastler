462c21441e8b268c97cbfa63dcf5f9d77deebdaf
Sebastian Benitez <jsb1439@hotmail.com>
2015-03-29 23:32:01 -0500

    actualizacion

---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\Users\SEBAS\Desktop\hastler\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\Users\SEBAS\Desktop\hastler\platforms\android\ant-build\AndroidManifest.xml : \
+c:\Users\SEBAS\Desktop\hastler\platforms\android\AndroidManifest.xml \
