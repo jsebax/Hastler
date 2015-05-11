0d90771b7d185240dd71a74a34b5e0f825ef6118
Anderson Cano <anderson.y312@gmail.com>
2015-05-11 16:50:27 -0500

    Mejora del pom del servidor para el despliegue

---
+++ server/HastlerServer/src/main/java/hastler/Application.java
@@ -0,0 +1,25 @@
+package hastler;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        
+        
+        
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        /* System.out.println("Let's inspect the beans provided by Spring Boot:");
+         * for (String beanName : beanNames) {
+         *  System.out.println(beanName);
+        }*/
+    }
+
+}
